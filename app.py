9c349350f4f32907345003f3772ce00be6128269
gturricepa <gturri@cepasafedrive.com>
2024-05-13 14:02:40 -0300

    [feat]: create endpoint to read xlsx

--- app.py
+++ app.py
@@ -9,12 +9,20 @@ CORS(app)
 @app.route('/', methods=['GET'])
 def home():
 
-    return 'server is running', 200
+    return 'Server is running', 200
 
 
-@app.route('/getuser', methods=['POST'])
-def return_user():
-
-    return jsonify({"data": "data here"}), 200
+@app.route('/getdata', methods=['POST'])
+def return_xls():
+    if 'xlsFile' not in request.files:
+        return jsonify({'error': 'Nenhum arquivo enviado.'}), 400
+    
+    xls_file = request.files['xlsFile']
+    try:
+        df = pd.read_excel(xls_file)
+        print(df.head)
+        return jsonify({'success': 'Arquivo XLS recebido e processado com sucesso.'}), 200
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
 
 app.run(port=5000, host='0.0.0.0', debug=True)
